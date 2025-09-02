6dfa8fef1db3d31a083b992959c0980fb66f56bd
himath <himathcode100@gmail.com>
2025-09-02 10:21:15 +0530

    add repository

--- src/main/java/com/example/demo/model/student.java
+++ src/main/java/com/example/demo/model/student.java
@@ -10,7 +10,7 @@ public class student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private  String name;
     private  String email;
 
