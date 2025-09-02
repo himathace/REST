5a9981984329c5eb39354e5a89c1cc24a7e634cd
himath <himathcode100@gmail.com>
2025-09-02 08:53:21 +0530

    add student entity

---
+++ src/main/java/com/example/demo/model/student.java
@@ -0,0 +1,29 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private  String name;
+    private  String email;
+
+    public student(){}
+
+    public student(String name,String email){
+        this.name=name;
+        this.email=email;
+    }
+
+
+
+
+
+
+}
