bec03b1a85fd2c128f9d8156d2a19d8276109cbf
himath <himathcode100@gmail.com>
2025-09-06 18:36:56 +0530

    feat: add validations

--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -4,13 +4,30 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.data.repository.NoRepositoryBean;
 
 @Entity
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id;
-    public String name;
-    public String email;
+    private Long id;
+
+    @NotBlank(message = "title is required")
+    @Size(max = 20,message = "title must be within 100 characters")
+    private String title;
+
+    @NotBlank(message = "description is required")
+    private String description;
+
+    @NotBlank(message = "description is required")
+    @Future(message = "deadline must be feture date")
+    private String deadline;
+
+    @NotBlank(message = "description is required")
+    private String status;
 
 }
